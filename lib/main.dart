50ea1f791be0b6f54cf7227e42fe291783c697e7
conrad96 <conrad.mugisha@gmail.com>
2018-09-04 20:40:49 +0300

    added a button

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,28 @@
-import 'package:flutter/material.dart';
+  import 'package:flutter/material.dart';
 
-//void main() => runApp(MyApp());
-main(){
-  runApp(MyApp());
-}
+  void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget
-{
-   Widget  build(context)
+  class MyApp extends StatelessWidget
   {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Conrad's App"),
-        ),//appBar
-        body: Card(child: Column(children: <Widget>[
-        Image.asset('assets/me.jpg'),
-        Text('Conrad Mugisha')
-        ]))
-      ),//scaffold
-    );
+     Widget  build(context)
+    {
+      return MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text("Conrad's App"),
+          ),//appBar
+          body: Column(
+            children: [
+              RaisedButton(
+              onPressed: () {},
+              child: Text("Click Here")
+              )
+            ,Card (child: Column(children: <Widget>[
+          Image.asset('assets/me.jpg'),
+          Text('Conrad Mugisha')
+          ]))] )
+        ),//scaffold
+      );
 
   }
 
